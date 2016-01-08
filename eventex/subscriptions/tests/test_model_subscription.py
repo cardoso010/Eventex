e3ee78e65ab7a41b12ab6fb5eedcd49224012d95
Gabriel Cardoso <gabriel.cardoso010@gmail.com>
2016-01-08 18:30:20 -0200

    Persiste a inscrição

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Gabriel Cardoso',
+            cpf='12345678901',
+            email='gabriel@cardoso.com.br',
+            phone='00-00000000'
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription mus have an outo created_at attr"""
+        self.assertIsInstance(self.obj.created_at, datetime)
