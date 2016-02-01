5ca9e45870ab2ac3228cf4bb3da1c6de32fe1737
Gabriel Cardoso <gabriel.cardoso010@gmail.com>
2016-02-01 17:04:41 -0200

    Extrai urls hardcoded

--- eventex/subscriptions/tests/test_mail_subscribe.py
+++ eventex/subscriptions/tests/test_mail_subscribe.py
@@ -1,12 +1,13 @@
 from django.core import mail
 from django.test import TestCase
+from django.shortcuts import resolve_url as r
 
 
 class SubscribePostValid(TestCase):
     def setUp(self):
         data = dict(name='Gabriel Cardoso', cpf='12345678901',
                     email='gabriel_cardoso010@yahoo.com.br', phone='00-0000-0000')
-        self.resp = self.client.post('/inscricao/', data)
+        self.resp = self.client.post(r('subscriptions:new'), data)
         self.email = mail.outbox[0]
 
     def test_subscription_email_subject(self):
