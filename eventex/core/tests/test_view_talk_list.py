4b025b42e48f97ade59a8bc634795469b5718a5d
Gabriel Cardoso <gabriel.cardoso010@gmail.com>
2016-02-23 17:55:18 -0300

    Course como Abstract Base Class

--- eventex/core/tests/test_view_talk_list.py
+++ eventex/core/tests/test_view_talk_list.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
-from eventex.core.models import Talk, Speaker
+from eventex.core.models import Talk, Speaker, Course
 
 
 class TalkListGet(TestCase):
@@ -10,12 +10,16 @@ class TalkListGet(TestCase):
         t2 = Talk.objects.create(title='Título da Palestra', start='13:00',
                                  description='Descrição da palestra.')
 
+        c1 = Course.objects.create(title='Título do Curso', start='09:00',
+                                 description='Descrição do curso.', slots=20)
+
         speaker = Speaker.objects.create(name='Gabriel Cardoso',
                                          slug='gabriel-cardoso',
                                          website='http://gabrielcardoso.net')
 
         t1.speakers.add(speaker)
         t2.speakers.add(speaker)
+        c1.speakers.add(speaker)
 
         self.resp = self.client.get(r('talk_list'))
 
@@ -30,9 +34,12 @@ class TalkListGet(TestCase):
             (2, 'Título da Palestra'),
             (1, '10:00'),
             (1, '13:00'),
-            (2, '/palestrantes/gabriel-cardoso/'),
-            (2, 'Gabriel Cardoso'),
-            (2, 'Descrição da palestra.')
+            (3, '/palestrantes/gabriel-cardoso/'),
+            (3, 'Gabriel Cardoso'),
+            (2, 'Descrição da palestra.'),
+            (1, 'Título do Curso'),
+            (1, '09:00'),
+            (1, 'Descrição do curso.')
         ]
 
         for count, expect in contents:
@@ -40,7 +47,7 @@ class TalkListGet(TestCase):
                 self.assertContains(self.resp, expect, count)
 
     def test_context(self):
-        variables = ['morning_talks', 'afternoon_talks']
+        variables = ['morning_talks', 'afternoon_talks', 'courses']
 
         for key in variables:
             with self.subTest():
